10a11d9e0458e5a7a78d1b6fe524c4ffc6d8466b
Madho <madhosingh@Vebuin.com>
2024-03-19 16:41:31 +0530

    Student Crud Operation MVC

---
+++ StudentRecordManagementSystem/Utility/ConnectionString.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentRecordManagementSystem.Utility
+{
+    public static class ConnectionString
+    {
+
+        private static string cName = "Data Source= APEX-75.;  Initial Catalog=StudentManagement;Integrated Security=false;Trusted_Connection=yes;";
+
+        public static string CName { get => cName; }
+    }
+}
